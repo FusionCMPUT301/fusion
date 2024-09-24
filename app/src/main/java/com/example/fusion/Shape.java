94d6530bd6dd992193116107f2bf979a16a8555e
Sehej Brar <sehejpur@ualberta.ca>
2024-09-24 17:48:55 -0600

    Added colour

--- app/src/main/java/com/example/fusion/Shape.java
+++ app/src/main/java/com/example/fusion/Shape.java
@@ -3,7 +3,7 @@ package com.example.fusion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Green";
+    private String color = "Blue";
 
 
     public Shape(int X, int Y) {
