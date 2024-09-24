b7b2354f94dd23ac3ef9e6c923c2c4e3a1e804e4
Sehej Brar <150099256+sehejb@users.noreply.github.com>
2024-09-24 17:40:03 -0600

    Update Shape.java
    Added green as a color.

--- app/src/main/java/com/example/fusion/Shape.java
+++ app/src/main/java/com/example/fusion/Shape.java
@@ -3,6 +3,8 @@ package com.example.fusion;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "Green";
+
 
     public Shape(int X, int Y) {
         this.x = X;
