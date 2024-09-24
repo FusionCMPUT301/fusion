ab2ad359e581d73135ab1d780654c61a5ac50b97
Nimi <takinroy@ualberta.ca>
2024-09-24 17:47:56 -0600

    hey

--- app/src/main/java/com/example/fusion/Shape.java
+++ app/src/main/java/com/example/fusion/Shape.java
@@ -3,7 +3,6 @@ package com.example.fusion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "red";
 
     public Shape(int X, int Y) {
         this.x = X;
