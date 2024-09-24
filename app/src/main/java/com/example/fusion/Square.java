dfe4f8109fbb2fa2143aa8ad91057ae4b5299f13
Sehej Brar <sehejpur@ualberta.ca>
2024-09-24 17:38:05 -0600

    Add a color

--- app/src/main/java/com/example/fusion/Square.java
+++ app/src/main/java/com/example/fusion/Square.java
@@ -3,6 +3,7 @@ package com.example.fusion;
 public class Square extends Shape {
     private int X;
     private int Y;
+    private String color = "Red"
     public Square(int X, int Y) {
         super(X, Y);
     }
