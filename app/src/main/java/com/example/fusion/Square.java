40f6af513502ee697bdf5a44ebb6b73c6609c452
Sehej Brar <sehejpur@ualberta.ca>
2024-09-24 20:23:18 -0600

    merge and colour change

--- app/src/main/java/com/example/fusion/Square.java
+++ app/src/main/java/com/example/fusion/Square.java
@@ -3,7 +3,7 @@ package com.example.fusion;
 public class Square extends Shape {
     private int X;
     private int Y;
-    private String color = "Green"
+    private String color = "Green";
     public Square(int X, int Y) {
         super(X, Y);
     }
