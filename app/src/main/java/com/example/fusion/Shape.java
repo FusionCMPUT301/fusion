7300d0ec8933287d1d966c04f9511890b4b57c81
Sehej Brar <sehejpur@ualberta.ca>
2024-09-24 20:22:30 -0600

    merge and colour change

--- app/src/main/java/com/example/fusion/Shape.java
+++ app/src/main/java/com/example/fusion/Shape.java
@@ -3,7 +3,7 @@ package com.example.fusion;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Red";
+    private String color = "Green";
 
 
     public Shape(int X, int Y) {
